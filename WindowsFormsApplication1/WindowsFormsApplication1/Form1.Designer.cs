0e279a8761c288a61d0ea9c5b0133c17eab019de
Harrison <mvharrison@labs.neit.edu>
2018-01-23 19:35:51 -0500

    Added writing into Google SpreadSheets

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,33 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.columnHeader0 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.txtBoxID = new System.Windows.Forms.TextBox();
+            this.txtBoxItem = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // listView1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(33, 42);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(655, 381);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader0,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(36, 96);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(855, 344);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader0
+            // 
+            this.columnHeader0.Text = "Student";
+            this.columnHeader0.Width = 180;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Lab Monitor";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Student ID";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Time In";
+            this.columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Time Out";
+            this.columnHeader4.Width = 120;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Equipment";
+            this.columnHeader5.Width = 180;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(971, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 71);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBoxName
+            // 
+            this.txtBoxName.Location = new System.Drawing.Point(971, 188);
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxName.TabIndex = 4;
+            this.txtBoxName.TextChanged += new System.EventHandler(this.txtBoxName_TextChanged);
+            // 
+            // txtBoxID
+            // 
+            this.txtBoxID.Location = new System.Drawing.Point(971, 214);
+            this.txtBoxID.Name = "txtBoxID";
+            this.txtBoxID.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxID.TabIndex = 5;
+            // 
+            // txtBoxItem
+            // 
+            this.txtBoxItem.Location = new System.Drawing.Point(971, 240);
+            this.txtBoxItem.Name = "txtBoxItem";
+            this.txtBoxItem.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxItem.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(761, 510);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(1207, 510);
+            this.Controls.Add(this.txtBoxItem);
+            this.Controls.Add(this.txtBoxID);
+            this.Controls.Add(this.txtBoxName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader0;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.TextBox txtBoxID;
+        private System.Windows.Forms.TextBox txtBoxItem;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -15,18 +15,32 @@ using System.Threading;
 
 namespace WindowsFormsApplication1
 {
+    
     public partial class Form1 : Form
     {
+        
+
         // If modifying these scopes, delete your previously saved credentials
         // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
-        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        static string[] Scopes = { SheetsService.Scope.Spreadsheets};
         static string ApplicationName = "Google Sheets API .NET Quickstart";
+        int rowStart = 5;
 
+        List<ListViewItem> currentlyRentedItemList = new List<ListViewItem>();
 
         public Form1()
         {
             InitializeComponent();
 
+            LoadItems();
+            
+
+            
+        }
+
+        private void LoadItems()
+        {
+            int numberOfColumns = 5;
 
             GoogleCredential credential;
 
@@ -64,18 +78,19 @@ namespace WindowsFormsApplication1
             if (values != null && values.Count > 0)
             {
                 Console.WriteLine("Name, Major");
+                int rowCounter = 0;
                 foreach (var row in values)
                 {
-                    // Print columns A and E, which correspond to indices 0 and 4.
-                    Console.WriteLine("{0}, {1}", row[0], row[4]);
+                    currentlyRentedItemList.Add(new ListViewItem(row[0].ToString()));
+                    for (int i = 1; i <= numberOfColumns;  i++)
+                    {
+                        currentlyRentedItemList[rowCounter].SubItems.Add(row[i].ToString());
+                    }
                     
-                    richTextBox1.Text += (row[0] + " ");
-                    richTextBox1.Text += (row[1] + " ");
-                    richTextBox1.Text += (row[2] + " ");
-                    richTextBox1.Text += (row[3] + " ") ;
-                    richTextBox1.Text += (row[4] + " ");
-                    richTextBox1.Text += (row[5] + " \n");
+                    rowCounter++;
                 }
+                listView1.Items.AddRange(currentlyRentedItemList.ToArray());
+
             }
             else
             {
@@ -83,11 +98,83 @@ namespace WindowsFormsApplication1
             }
             Console.Read();
 
-
-            
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GoogleCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
+
+                credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
+
+                Console.WriteLine("Credential file saved to: " + credPath);
+
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            
+            // Define request parameters.
+            String spreadsheetId = "1Ox1qngNAtKSIkWHBaORz9HtyugXflEL9eclDLt09BWo";
+            String range = "Winter 2018!A" + rowStart.ToString() + ":F";
+            
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+            ValueRange response = request.Execute();
+            IList<IList<Object>> values = response.Values;
+
+            string rowToInsert = (rowStart + values.Count()).ToString();
+
+            String newRange = "Winter 2018!A" + rowToInsert + ":F" + rowToInsert;
+            
+            ValueRange valueRange = new ValueRange();
+            valueRange.MajorDimension = "ROWS";//"ROWS";//COLUMNS
+
+            var oblist = new List<object>() { txtBoxName.Text, "Michael Harrison", txtBoxID.Text, DateTime.Now.ToString("g"), "", txtBoxItem.Text };
+
+            ListViewItem newItem = new ListViewItem(txtBoxName.Text);
+            newItem.SubItems.Add("Michael Harrison");
+            newItem.SubItems.Add(txtBoxID.Text);
+            newItem.SubItems.Add(DateTime.Now.ToString("g"));
+            newItem.SubItems.Add("");
+            newItem.SubItems.Add(txtBoxItem.Text);
+            listView1.Items.Add(newItem);
+
+            valueRange.Values = new List<IList<object>> { oblist };
+
+            SpreadsheetsResource.ValuesResource.UpdateRequest update = service.Spreadsheets.Values.Update(valueRange, spreadsheetId, newRange);
+            update.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+            UpdateValuesResponse result2 = update.Execute();
+        }
+
+        private void txtBoxName_TextChanged(object sender, EventArgs e)
         {
 
         }
